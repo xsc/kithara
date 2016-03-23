e555d90e178dfd143388a27805eb51a33f1849e7
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-03-23 22:23:27 +0100

    add kithara client properties.

--- src/kithara/rabbitmq/connection.clj
+++ src/kithara/rabbitmq/connection.clj
@@ -2,7 +2,25 @@
   (:require [kithara.config :as config])
   (:import [net.jodah.lyra Connections ConnectionOptions]
            [net.jodah.lyra.config Config]
-           [com.rabbitmq.client Connection]))
+           [com.rabbitmq.client Connection]
+           [com.rabbitmq.client.impl AMQConnection]))
+
+(def ^:private platform
+  (let [prop #(System/getProperty (name %))]
+    (format "Clojure %s on Java %s (%s)"
+            (clojure-version)
+            (prop :java.version)
+            (prop :java.vm.name))))
+
+(def ^:private client-properties
+  (into
+    {}
+    [(AMQConnection/defaultClientProperties)
+     {"product"     "kithara"
+      "copyright"   "Copyright (c) 2016 Yannick Scherer"
+      "information" "https://github.com/xsc/kithara"
+      "platform"    platform
+      "version"     "0.1.x"}]))
 
 (defn open
   "Create a new RabbitMQ connection backed by Lyra's recovery facilities.
@@ -10,9 +28,10 @@
    options."
   (^com.rabbitmq.client.Connection
     [options]
-    (open
-      (config/connection options)
-      (config/behaviour options)))
+    (let [options (update options :properties #(merge client-properties %))]
+      (open
+        (config/connection options)
+        (config/behaviour options))))
   (^com.rabbitmq.client.Connection
     [^ConnectionOptions connection-options ^Config config]
     (Connections/create connection-options config)))
