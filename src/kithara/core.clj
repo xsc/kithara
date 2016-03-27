6ff0bd6109fe2863a2ac0df8ff46e764ecb64740
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-03-27 20:50:50 +0200

    adjust facade.

--- src/kithara/core.clj
+++ src/kithara/core.clj
@@ -2,23 +2,23 @@
   "Public API for simple RabbitMQ consumer creation."
   (:require [kithara.components
              [base-consumer :as base-consumer]
-             [channel-consumer :as channel-consumer]
-             [connected-consumer :as connected-consumer]
-             [publisher :as publisher]
-             [queue-consumer :as queue-consumer]]
+             channel-wrapper
+             connection-wrapper
+             publisher
+             queue-wrapper]
             [potemkin :refer [import-vars]]))
 
 (import-vars
   [kithara.components.base-consumer
    consumer]
-  [kithara.components.channel-consumer
+  [kithara.components.channel-wrapper
    with-channel
    with-prefetch-channel]
-  [kithara.components.connected-consumer
+  [kithara.components.connection-wrapper
    with-connection]
   [kithara.components.publisher
    publisher]
-  [kithara.components.queue-consumer
+  [kithara.components.queue-wrapper
    with-queue
    with-durable-queue
    with-server-named-queue])
