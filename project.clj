e6ce7f0740f7e72e7ae6320c757c595c6312348f
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-03-27 21:39:08 +0200

    produce two sets of docs for consumer building and RabbitMQ fns.

--- project.clj
+++ project.clj
@@ -12,16 +12,29 @@
                  [peripheral "0.5.0"]
                  [flake "0.3.1"]
                  [potemkin "0.4.3"]]
-  :profiles {:dev {:dependencies [[ch.qos.logback/logback-classic "1.1.6"]
-                                  [org.slf4j/slf4j-api "1.7.18"]]
-                   :plugins [[lein-codox "0.9.4"]]
-                   :codox {:project {:name "kithara"}
-                           :metadata {:doc/format :markdown}
-                           :source-uri "https://github.com/xsc/kithara/blob/v{version}/{filepath}#L{line}"
-                           :output-path "doc"
-                           :namespaces [kithara.core
-                                        kithara.config
-                                        kithara.protocols
-                                        #"^kithara\.patterns\.[a-z\-]+"
-                                        #"^kithara\.middlewares\.[a-z\-]+"]}}}
+  :profiles {:dev
+             {:dependencies [[ch.qos.logback/logback-classic "1.1.6"]
+                             [org.slf4j/slf4j-api "1.7.18"]]}
+             :codox
+             [:dev
+              {:plugins [[lein-codox "0.9.4"]]
+               :codox {:project {:name "kithara"}
+                       :metadata {:doc/format :markdown}
+                       :source-uri "https://github.com/xsc/kithara/blob/v{version}/{filepath}#L{line}"}}]
+             :codox-consumers
+             [:codox
+              {:codox {:output-path "doc"
+                       :namespaces [kithara.core
+                                    kithara.config
+                                    kithara.protocols
+                                    #"^kithara\.patterns\.[a-z\-]+"
+                                    #"^kithara\.middlewares\.[a-z\-]+"]}}]
+             :codox-rabbitmq
+             [:codox
+              {:codox {:output-path "doc/rabbitmq"
+                       :namespaces [#"kithara\.rabbitmq\.[a-z\-]+"]}}]}
+  :aliases {"codox"
+            ["do"
+             "with-profile" "+codox-consumers" "codox,"
+             "with-profile" "+codox-rabbitmq" "codox"]}
   :pedantic? :abort)
