4fa73d9bd982cf890f18ad5161af8516d560c40e
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-04-17 17:10:06 +0200

    allow for supplying of message verifiers using metadata in 'stack-elements'.

--- test/kithara/test/stack.clj
+++ test/kithara/test/stack.clj
@@ -7,21 +7,25 @@
   [& stack-gens]
   (gen/fmap
     (fn [results]
-      {:forms    (vec (mapcat :forms results))
-       :build-fn #(reduce
-                    (fn [value {:keys [build-fn]}]
-                      (if build-fn
-                        (build-fn value %2)
-                        value))
-                    %1 results)})
+      {:forms     (vec (mapcat :forms results))
+       :verifiers (into {} (map :verifiers results))
+       :build-fn  #(reduce
+                     (fn [value {:keys [build-fn]}]
+                       (if build-fn
+                         (build-fn value %2)
+                         value))
+                     %1 results)})
     (apply gen/tuple stack-gens)))
 
 (defmacro stack-elements
   [bindings & options]
   `(gen/elements
-     [~@(for [form options]
-          `{:forms    [(quote ~(cons (first form) (next (next form))))]
-            :build-fn (fn [~@bindings] ~form)})]))
+     [~@(for [form options
+              :let [verifiers (-> form meta :verifiers)]]
+          `{:forms     [(quote ~(cons (first form) (next (next form))))]
+            :verifiers ~(into {} (for [verifier verifiers]
+                                   `[(quote ~verifier) ~verifier]))
+            :build-fn  (fn [~@bindings] ~form)})]))
 
 (defmacro optional-stack-elements
   [bindings & options]
