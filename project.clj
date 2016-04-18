afac0aa8248375fd9a366afcffeb0e8ed570945d
Yannick Scherer <yannick.scherer@stylefruits.de>
2016-04-18 16:45:51 +0200

    Version 0.1.3

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject kithara "0.1.3-SNAPSHOT"
+(defproject kithara "0.1.3"
   :description "A Clojure Library for Reliable RabbitMQ Consumers."
   :url "https://github.com/xsc/kithara"
   :license {:name "MIT License"
